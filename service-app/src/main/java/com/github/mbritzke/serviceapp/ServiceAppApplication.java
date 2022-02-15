2040c6ae5a0d161fc758aa8728be3b6f6388b41a
mbritzke <matheusbritzke@gmail.com>
2022-02-15 16:49:55 -0300

    First commit

---
+++ service-app/src/main/java/com/github/mbritzke/serviceapp/ServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.mbritzke.serviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAppApplication.class, args);
+	}
+
+}
